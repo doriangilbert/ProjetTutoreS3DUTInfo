0f1c03f35f712c707cd236b5093fda556d1a072b
dodo2308 <dodo23082001@gmail.com>
2021-01-10 10:12:36 +0100

    First Commit

---
+++ vues/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr-fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Ma superbe page</title>
+    </head>
+    <body>
+        <h1>Ceci est un grand titre.</h1>
+        <h2>Ceci est un titre plus petit que le précédent.</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, iste? Nemo, omnis ipsa! Vel quibusdam explicabo, id, ducimus nemo veritatis cumque officia quos debitis placeat dignissimos quidem reiciendis iusto possimus.
+        </p>
+    </body>
+</html>
