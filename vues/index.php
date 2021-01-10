9557da48541235b00843b79085e2e9c7d995b92b
Matt504 <matteoserrano84@gmail.com>
2021-01-10 21:27:36 +0100

    Premier commit : démarrage du projet avec bootstrap, création du menu et page d'accueil

--- vues/index.php
+++ vues/index.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html lang="fr-fr">
-    <head>
-        <meta charset="utf-8">
-        <title>Ma superbe page</title>
-    </head>
+<head>
+    <meta charset="utf-8">
+    <title>Ma superbe page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+</head>
     <body>
         <h1>Ceci est un grand titre.</h1>
         <h2>Ceci est un titre plus petit que le précédent.</h2>
